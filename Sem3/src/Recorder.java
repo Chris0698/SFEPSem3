f6f6498a1a88d3c298f89239853a2696160de05d
jonathan.watt <w16012960@northumbria.ac.uk>
2018-02-01 15:53:28 +0000

    Created basic class with methods

--- Sem3/src/Recorder.java
+++ Sem3/src/Recorder.java
@@ -1,4 +1,52 @@
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Recorder {
-
+	
+	private RecorderUI recorderUI;
+	private Array records;
+	
+	
+	/*
+	public static void main(String[] args) {
+		new Recorder();
+	}
+	*/
+	
+	public Recorder(){
+		recorderUI = new RecorderUI();
+		recorderUI.display();
+	}
+	
+	public void display(){
+		
+	}
+	
+	public void setSize(int numStudents){
+		
+	}
+	
+	public void getRecord(int num){
+		
+	}
+	
+	public void setName(String name){
+		
+	}
+	
+	public void setMark(int mark){
+		
+	}
+	
+	public void getMark(){
+		
+	}
+	
+	public void calculateStatistics(){
+		
+	}
+	
+	public void showStatistics(){
+		
+	}
 }
